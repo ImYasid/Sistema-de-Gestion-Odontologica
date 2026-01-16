0a48b4f0d772c32e7926de6c4ec6e78446c6abbe
Erickezro <95935423+Erickezro@users.noreply.github.com>
2026-01-15 23:02:08 -0500

    :sparkles: initialize pacientes-service with basic configuration and structure

---
+++ BackEnd/pacientes-service/src/main/java/com/endonova/pacientesservice/PacientesServiceApplication.java
@@ -0,0 +1,13 @@
+package com.endonova.pacientesservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PacientesServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PacientesServiceApplication.class, args);
+    }
+
+}
